453d261e0ac346a0d9ab9c012ea6ed7be0f8a972
Geordie Ayers <55464996+GeordieAyers@users.noreply.github.com>
2022-11-10 14:16:29 -0500

    API post and request rest functional

--- api/server.js
+++ api/server.js
@@ -22,12 +22,26 @@ const mongoAtlasUri =
         console.log("could not connect");
       }
 
-const Todo = require('./models/Todo');
+const Todo = require('./models/Todo'); // importing our model to use in the application
 
+// get request for connecting to the DB
 app.get('/todos', async (req, res) => {
     const todos = await Todo.find(); // find our todos using our models connected to our mongoose DB
 
     res.json(todos); // pass all the todos back to the file here
 });
+
+// post request for sending a new  singular todo
+app.post('/todos/new', async (req, res) => {
+    const todo = new Todo({ // build new todo request using the body of the req
+        text: req.body.text
+    });
+
+    todo.save();
+
+    res.json(todo); // pass all the todos back to the file here
+});
+
+
       
 app.listen(3001, ()=> console.log("server started on port 3001"));
